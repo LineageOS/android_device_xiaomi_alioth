1af61b8353310f8f9662e97e8ef32181d061b071
Albert I <kras@raphielgang.org>
2021-06-13 18:33:36 +0800

    alioth: Update platform info from LA.UM.9.12.r1-08300-SMxx50.0
    - Import mixer_paths_overlay_dynamic
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I5483ae59edd031e381a61619f8f57caaca4cabd6

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ TARGET_ENABLE_AUDIO_ULL := true
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_overlay_dynamic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_dynamic.xml \
     $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
