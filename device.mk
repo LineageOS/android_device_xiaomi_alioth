8a41522f6b1cd7c66bda64dc8a1e0ef52ca23fd4
Albert I <kras@raphielgang.org>
2021-07-25 05:23:52 +0800

    alioth: Remove touchfeature from sensor HALs loading list
    * Move sensors/hals.conf to device tree to make it possible
    
    Change-Id: I76010d17f1f20b46a5f27749c02b42e2afffd674
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
