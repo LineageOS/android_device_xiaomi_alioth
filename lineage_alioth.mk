24a80eb36a2b0248fd4f41626285793db604e839
althafvly <althafvly@gmail.com>
2021-07-27 00:44:23 +0530

    alioth: inherit the virtual A/B device base configuration
    refs: https://source.android.com/devices/tech/ota/virtual_ab/implement
    
    Change-Id: Ib1d2c29ca52abc140d6023a64b0e6cafc6cea7a4

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
