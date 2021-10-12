47ff122bdb40609f10f03ccb2c50b5661ed3aed9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 18:41:32 +0200

    alioth: Patch vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    The modified library NOPs out the instruction to change binder
    threadpool, allowing the library to load and camera to work on S.
    
    Change-Id: I1ef0ed29e4a2eb820826e5d7df1fa53c9e3df584

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
