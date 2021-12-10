836c8ea6c0e8ac9c3759dd561e55317c7c4e46cf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-10 01:01:53 +0100

    alioth: Update blobs from V12.5.4.0.RKHMIXM
    Change-Id: I52f47ea15140af63ba9f0b0a7b9d237a25abdbdb

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,19 +9,6 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t aliothcn_info = {
-    .hwc_value = "CN",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "alioth",
-    .marketname = "K40",
-    .model = "M2012K11AC",
-    .build_fingerprint = "Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys",
-
-    .nfc = true,
-};
-
 static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
@@ -30,12 +17,12 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/aliothin:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys",
+    .build_fingerprint = "Mi/aliothin/aliothin:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
 
     .nfc = false,
 };
 
-static const variant_info_t alioth_info = {
+static const variant_info_t alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -43,14 +30,27 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys",
+    .build_fingerprint = "POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
+
+    .nfc = true,
+};
+
+static const variant_info_t alioth_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "alioth",
+    .marketname = "K40",
+    .model = "M2012K11AC",
+    .build_fingerprint = "Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
 
     .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
-    aliothcn_info,
     aliothin_info,
+    alioth_global_info,
     alioth_info,
 };
 
