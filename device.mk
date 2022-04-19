da7866fe93d0086ba5751bcdb5f46a4fbf9f692c
althafvly <althafvly@gmail.com>
2022-04-19 14:27:01 +0530

    Revert^2 "alioth: Commonize A/B stuff"
    This reverts commit 5b244d60b4954cf5cc5a9b196f015c82a076cc2a.
    
    Change-Id: I60e525c287fe3dfa82b4839ba3ea0faab1013516

--- device.mk
+++ device.mk
@@ -4,15 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# A/B
+TARGET_IS_VAB := true
+
 # Audio
 TARGET_ENABLE_AUDIO_ULL := true
 
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
-
-# Inherit virtual_ab_ota product
-$(call inherit-product, \
-    $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -28,36 +27,6 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 30
-
-# A/B
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
-PRODUCT_PACKAGES += \
-  update_engine \
-  update_engine_sideload \
-  update_verifier
-
-PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
 
 # Audio configs
 PRODUCT_COPY_FILES += \
