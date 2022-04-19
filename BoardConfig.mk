673dece46afdb157d63f616872e16f92497a9581
althafvly <althafvly@gmail.com>
2022-04-19 14:58:35 +0530

    alioth: rootdir: add device fstab
    Change-Id: Ibff2be7b7216281a874d0081470e843d9dfb96f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,5 +56,8 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 4559208448 # ( BOARD_SUPER_PARTITION_SIZE /
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
 # Inherit from the proprietary version
 include vendor/xiaomi/alioth/BoardConfigVendor.mk
