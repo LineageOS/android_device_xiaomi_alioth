121915983aa3ab5cce1d2b32cc5c318495af8556
althafvly <althafvly@gmail.com>
2022-04-29 00:17:13 +0530

    alioth: Update blobs from V12.5.6.0.RKHMIXM
    Change-Id: I423a31f361b4ffef7f985d7d1c7bac526046df45

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -17,7 +17,7 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/aliothin:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
+    .build_fingerprint = "Mi/aliothin/aliothin:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys",
 
     .nfc = false,
 };
@@ -30,7 +30,7 @@ static const variant_info_t alioth_global_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
+    .build_fingerprint = "POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -43,7 +43,7 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "K40",
     .model = "M2012K11AC",
-    .build_fingerprint = "Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys",
 
     .nfc = true,
 };
