8a551cc6c5f0c9f88df13f42a706696914cb5004
SahilSonar <sss.sonar2003@gmail.com>
2022-05-01 06:55:03 +0200

    alioth: Update to Android 12 blobs
    Change-Id: I3dca030113c45ceb7e6f580a2fa34b59ee37c789

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.6.0.RKHMIXM release-keys"
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V13.0.2.0.SKHMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/RKQ1.200826.002/V13.0.2.0.SKHMIXM:user/release-keys
