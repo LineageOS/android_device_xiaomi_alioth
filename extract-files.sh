76d3885fb818e0ee229545d5186dc236733afc75
althafvly <althafvly@gmail.com>
2022-05-09 18:26:59 +0530

    alioth: camera: Use randomly generated config
    Change-Id: Ib3f6f1526e2e0b7db23bb34c5b15aec1b0d338c2

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/hw/camera.qcom.so)
+            sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
+            ;;
+        vendor/lib64/hw/camera.qcom.so)
+            sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
