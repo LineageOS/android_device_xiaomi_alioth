86a3a127fd5ecc0faf6a35fe6b74a63bc2a2edf3
althafvly <althafvly@gmail.com>
2022-05-09 18:26:59 +0530

    alioth: camera: Use randomly generated config
    Change-Id: Ib3f6f1526e2e0b7db23bb34c5b15aec1b0d338c2

--- extract-files.sh
+++ extract-files.sh
@@ -10,6 +10,9 @@ function blob_fixup() {
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
+        vendor/lib64/hw/camera.qcom.so)
+            sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
+            ;;
     esac
 }
 
