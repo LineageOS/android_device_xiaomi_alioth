6b4e6b46ca22af86676edc54a4b7a58d7a950450
althafvly <althafvly@gmail.com>
2022-09-11 18:02:45 +0530

    alioth: Use certified fingerprint for each region.
    - Xiaomi scammed, its not RKQ1.200826.002.
    - Use recommended firmware fingerprint from here
      https://wiki.lineageos.org/devices/alioth/fw_update
    - Global fingeprint isn't certified for other regions.
    - Models with these SP passes with hacks: M2012K11AC_2022-02-01,
      M2012K11AG_2022-04-01, M2012K11AI_2022-04-01
    
    Change-Id: I7c0a133b22a9693b4969fad1ef1b6ebbe623c960

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -17,7 +17,7 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/aliothin:12/RKQ1.200826.002/V13.0.3.0.SKHMIXM:user/release-keys",
+    .build_fingerprint = "Mi/aliothin/aliothin:12/SKQ1.211006.001/V13.0.6.0.SKHINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -30,7 +30,7 @@ static const variant_info_t alioth_global_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_global/alioth:12/RKQ1.200826.002/V13.0.3.0.SKHMIXM:user/release-keys",
+    .build_fingerprint = "POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.3.0.SKHMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -43,7 +43,7 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "K40",
     .model = "M2012K11AC",
-    .build_fingerprint = "Redmi/alioth_global/alioth:12/RKQ1.200826.002/V13.0.3.0.SKHMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/alioth/alioth:12/SKQ1.211006.001/V13.0.5.0.SKHCNXM:user/release-keys",
 
     .nfc = true,
 };
