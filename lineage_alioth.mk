35aadb004dcff2ef01f65b145fc17a48da5f2225
SahilSonar <sss.sonar2003@gmail.com>
2022-09-22 16:05:11 +0530

    alioth: Update blobs from V13.0.6.0.SKHMIXM
    Change-Id: I6deb3b9e1535bf92cfe66b9e881a7b0578637cbe

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.3.0.SKHMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.6.0.SKHMIXM:user/release-keys
