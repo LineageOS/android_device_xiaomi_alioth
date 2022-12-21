d9a11f7d5794515e01f61f4eba5debde52c623dd
SahilSonar <sss.sonar2003@gmail.com>
2022-12-21 05:22:32 +0100

    alioth: Include alioth config fragment
    Change-Id: Ib88f5e7c43826609239c55bc0f8a87e05dcc63cd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
