d1a2f82ee95d78752d7a27e4517ea6e1227f38b6
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    alioth: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"hardware/qcom-caf/common/libqti-perfd-client",
+		"vendor/qcom/opensource/display",
+EOF
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
