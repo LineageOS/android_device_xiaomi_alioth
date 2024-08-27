91df698bf6bcaac8f2e3cd2175d80aea3516e963
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    alioth: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-# Copyright (C) 2021 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/etc/init/init.batterysecret.rc)
+            [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
         vendor/etc/libnfc-nci.conf)
+            [ "$2" = "" ] && return 0
             cat << EOF >> "${2}"
 ###############################################################################
 # Mifare Tag implementation
@@ -20,25 +22,40 @@ LEGACY_MIFARE_READER=1
 EOF
             ;;
         vendor/lib/hw/audio.primary.alioth.so)
+            [ "$2" = "" ] && return 0
             ${PATCHELF} --set-soname "audio.primary.alioth.so" "${2}"
             sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_alioth\.so\x00\x00\x00\x00\x00" "${2}"
             ;;
         vendor/lib/liba2dpoffload_alioth.so)
+            [ "$2" = "" ] && return 0
             ${PATCHELF} --set-soname "liba2dpoffload_alioth.so" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.fpc.default.so)
+            [ "$2" = "" ] && return 0
             ${PATCHELF} --set-soname "fingerprint.fpc.default.so" "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/hw/camera.qcom.so)
+            [ "$2" = "" ] && return 0
             sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
             ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -52,5 +69,6 @@ set -e
 export DEVICE=alioth
 export DEVICE_COMMON=sm8250-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
