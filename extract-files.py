73f1e3cb03136780226b133e274dc7a6b4b7b008
LuK1337 <priv.luk@gmail.com>
2025-01-21 13:25:21 +0100

    alioth: Reland "Switch to py extract utils"
    Change-Id: I48113a9f7e5fa60b721ea4ae0ea5c375745feb19

---
+++ extract-files.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .add_line_if_missing('LEGACY_MIFARE_READER=1'),
+    'vendor/lib/hw/audio.primary.alioth.so': blob_fixup()
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_alioth.so\x00\x00\x00\x00\x00',
+        ),
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .binary_regex_replace(
+            b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63',
+            b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74',
+        ),
+    'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'vendor/qcom/opensource/display',
+]
+
+module = ExtractUtilsModule(
+    'alioth',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8250-common', module.vendor)
+    utils.run()
